51b9f0e9ecb9415a3beca51d967c073860cef5c7
godu <duarte.goncalves@aeis.ch>
2020-11-16 08:17:52 +0100

    Add Basic Projects

---
+++ spelline-backend/src/main/java/ch/bbw/spellinebackend/SpellineBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.spellinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellineBackendApplication.class, args);
+	}
+
+}
---
+++ spelline-backend/src/test/java/ch/bbw/spellinebackend/SpellineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.spellinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
