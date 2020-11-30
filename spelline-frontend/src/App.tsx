5ef1f996520f81356d8a6ea1efb49c6925521d78
godu <duarte.goncalves@aeis.ch>
2020-11-30 09:21:42 +0100

    Add Basic Layouting of User-view

--- spelline-frontend/src/App.tsx
+++ spelline-frontend/src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import {AppBar, Button, makeStyles, Toolbar, Typography} from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="app">
+        <header className="app-header">
+          <AppBar position="static" >
+            <Toolbar>
+              <Typography variant="h6" className="title">
+                Spelline
+              </Typography>
+              <Button color="inherit"> Login</Button>
+            </Toolbar>
+          </AppBar>
+        </header>
+        <body>
+        <div className='upload-box'>
+          <form method="post" action="#" id="#">
+            <div className="files">
+              <input type="file" className="form-control" />
+            </div>
+          </form>
+        </div>
+        </body>
+      </div>
   );
 }
 
